f25ca340f181845ffc66912d7e85b74a3d805be2
boluo1997 <1109928059@qq.com>
2021-04-24 01:19:57 +0800

    选择排序

---
+++ src/main/java/algorithm/selectSort.java
@@ -0,0 +1,25 @@
+package algorithm;
+
+public class selectSort {
+
+    public static void main(String[] args) {
+        int nums[] = new int[]{2, 8, 99, 15, 73, 65, 42, 37, 65};
+        selectSort(nums);
+        for (int i : nums) {
+            System.out.println(i);
+        }
+    }
+
+    public static void selectSort(int nums[]) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+}
