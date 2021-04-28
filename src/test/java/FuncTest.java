30351b72b4d8894341812cab2231d3162705822b
boluo1997 <1109928059@qq.com>
2021-04-28 23:01:01 +0800

    测试用例修改

--- src/test/java/FuncTest.java
+++ src/test/java/FuncTest.java
@@ -4,14 +4,12 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
 import com.google.common.collect.ImmutableList;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.expressions.Window;
-import org.apache.spark.sql.types.StringType$;
 import org.junit.Assert;
 import org.junit.Test;
 import scala.Tuple2;
@@ -19,6 +17,13 @@ import scala.Tuple3;
 import work.Func;
 import work.My;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import static org.apache.spark.sql.functions.*;
+import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 
 public class FuncTest {
@@ -74,19 +79,56 @@ public class FuncTest {
         Dataset<Tuple3<String, String, Long>> ds = spark.createDataset(ImmutableList.of(
                 Tuple3.apply("2020-04-01 07:00", null, null),
                 Tuple3.apply("2020-04-01 08:00", "主营业务收入.收入.加液", 2L),
-                Tuple3.apply("2020-04-02 08:00", "主营业务收入.收入.加液", 2L),	// [4.1, 1]
-                Tuple3.apply("2020-04-03 08:00", "主营业务收入.收入.X", 4L),		// [4.2, 1]
-                Tuple3.apply("2020-04-05 16:00", "主营业务收入.收入.X", 4L),		// [4.3, 1],[4.4, 1]
-                Tuple3.apply("2020-04-06 08:00", "主营业务收入.收入.加液", 4L),	// [4.5, 1]
-                Tuple3.apply("2020-04-07 08:00", "主营业务收入.收入.加液", 2L),	// [4.6, 1]
-                Tuple3.apply("2020-04-09 07:00", null, null)					// [4.7, 1],[4.8, 1]
+                Tuple3.apply("2020-04-02 08:00", "主营业务收入.收入.加液", 2L),    // [4.1, 1]
+                Tuple3.apply("2020-04-03 08:00", "主营业务收入.收入.X", 4L),        // [4.2, 2]
+                Tuple3.apply("2020-04-05 16:00", "主营业务收入.收入.X", 4L),        // [4.3, 1],[4.4, 1]
+                Tuple3.apply("2020-04-06 08:00", "主营业务收入.收入.加液", 4L),    // [4.5, 1]
+                Tuple3.apply("2020-04-07 08:00", "主营业务收入.收入.加液", 2L),    // [4.6, 2]
+                Tuple3.apply("2020-04-09 07:00", null, null)                    // [4.7, 1],[4.8, 1]
         ), Encoders.tuple(Encoders.STRING(), Encoders.STRING(), Encoders.LONG()));
         Dataset<Row> df = ds
                 .withColumn("ex", My.分摊分成("timestamp(_1)", "_3", "_2",
-                        "if(_2=null,null,10)", "0.4", "0.8", "date('2020-04-09')")
+                        "if(isnull(_2),10,null)", "null", "null", "if(isnull(_2),8,null)")
                         .over(Window.orderBy("_1")))
-                .selectExpr("ex");
-        df.show(false);
+                .selectExpr("explode(ex) ex")
+                .selectExpr("ex.*");
+        assertEquals(8, df.count());
+        assertEquals(10L, df.agg(sum("amount")).first().getLong(0));
+        assertEquals(2L, df.where("date='2020-04-02'").first().getLong(1));
+        assertEquals(2L, df.where("date='2020-04-06'").first().getLong(1));
+
+        df = ds
+                .withColumn("ex", My.分摊分成("timestamp(_1)", "_3", "_2",
+                        "null",
+                        "if(isnull(_2),0.4,null)", "if(isnull(_2),0.8,null)",
+                        "if(isnull(_2),8,null)")
+                        .over(Window.orderBy("_1")))
+                .selectExpr("explode(ex) ex")
+                .selectExpr("ex.*");
+        assertEquals(8, df.count());
+        assertEquals(11L, df.agg(sum("amount")).first().getLong(0));
+        assertEquals(2L, df.where("date='2020-04-01'").first().getLong(1));
+        assertEquals(1L, df.where("date='2020-04-02'").first().getLong(1));
+        assertEquals(0L, df.where("date='2020-04-04'").first().getLong(1));
+        assertEquals(4L, df.where("date='2020-04-06'").first().getLong(1));
+        assertEquals(1L, df.where("date='2020-04-07'").first().getLong(1));
+
+        df = ds
+                .withColumn("ex", My.分摊分成("timestamp(_1)", "_3", "_2",
+                        "if(isnull(_2),4,null)",
+                        "if(isnull(_2),0.4,null)", "if(isnull(_2),0.8,null)",
+                        "if(isnull(_2),8,null)")
+                        .over(Window.orderBy("_1")))
+                .selectExpr("explode(ex) ex")
+                .selectExpr("ex.*");
+        assertEquals(8, df.count());
+        assertEquals(11L, df.agg(sum("amount")).first().getLong(0));
+        assertEquals(1L, df.where("date='2020-04-01'").first().getLong(1));
+        assertEquals(4L, df.where("date='2020-04-03'").first().getLong(1));
+        assertEquals(0L, df.where("date='2020-04-04'").first().getLong(1));
+        assertEquals(4L, df.where("date='2020-04-06'").first().getLong(1));
+        assertEquals(0L, df.where("date='2020-04-08'").first().getLong(1));
     }
+
 
 }
