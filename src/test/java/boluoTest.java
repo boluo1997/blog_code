0971fe6243da50b03420651b84670bf6938c8355
boluo1997 <1109928059@qq.com>
2021-05-05 17:19:47 +0800

    线程池

--- src/test/java/boluoTest.java
+++ src/test/java/boluoTest.java
@@ -1,16 +1,26 @@
 import boluo.basics.Note02_Enum;
+import boluo.basics.Note03_ThreadPoolManager;
 import com.google.common.collect.Lists;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class boluoTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(boluoTest.class);
+
     @Test
     public void func1(){
         int areaType = 3;
         if (Note02_Enum.PREFECTURE.eq(areaType)) {
-            System.out.println(Note02_Enum.PREFECTURE.getName());
+
+            logger.info(Note02_Enum.PREFECTURE.getName());
+            Note03_ThreadPoolManager.run(() -> {
+                // do something
+            });
+
         }
     }
 }
