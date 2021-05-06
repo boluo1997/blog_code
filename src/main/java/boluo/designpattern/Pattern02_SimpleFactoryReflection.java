6dcbadcfd86581a544c39b93dd64e1a6d428340e
boluo1997 <1109928059@qq.com>
2021-05-06 21:25:49 +0800

    简单工厂模式 -> 反射实现

---
+++ src/main/java/boluo/designpattern/Pattern02_SimpleFactoryReflection.java
@@ -0,0 +1,59 @@
+package boluo.designpattern;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 反射实现工厂模式
+ */
+public class Pattern02_SimpleFactoryReflection {
+
+    private static final Map<EnumProductType, Class> activityIdMap = new HashMap<>();
+
+    public static void addProductKey(EnumProductType EnumProduct, Class product) {
+        activityIdMap.put(EnumProduct, product);
+    }
+
+    public static activityOne product(EnumProductType type) throws IllegalAccessException, InstantiationException {
+        Class productClass = activityIdMap.get(type);
+        return (activityOne) productClass.newInstance();
+    }
+
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+        addProductKey(EnumProductType.activityOne, activityOne.class);
+        activityOne product = product(EnumProductType.activityOne);
+        System.out.println(product.toString());
+    }
+
+    public static class Product {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class activityOne extends Product {
+        private String store;
+
+        @Override
+        public String toString() {
+            return "activityOne{" +
+                    "store='" + store + '\'' +
+                    '}';
+        }
+    }
+
+    public static class activityTwo extends Product {
+        private String store;
+    }
+
+    public enum EnumProductType {
+        activityOne, activityTwo;
+    }
+
+}
