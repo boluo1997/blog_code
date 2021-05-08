2176b7002917d927cfcfee7e028560d00436be22
boluo1997 <1109928059@qq.com>
2021-05-08 22:02:06 +0800

    分摊分成兼容多个任务同时执行的情况

--- src/main/java/boluo/work/My.java
+++ src/main/java/boluo/work/My.java
@@ -231,26 +231,30 @@ public class My {
                         .add("固定金额", "long")
                         .add("非加液分成比例", "double")
                         .add("加液分成比例", "double")
-                        .add("总天数", "int");
+                        .add("结束日", "timestamp");
             }
 
             @Override
             public StructType bufferSchema() {
                 StructType aType = new StructType()
-                        .add("totalAmount", "long")			// a 固定总金额 0
-                        .add("totalDays", "int")				// d 总天数 1
-                        .add("noLiquidRate", "double")		// k 非加液利率 2
-                        .add("liquidRate", "double")			// q 加液利率 3
-                        .add("previousNoLiquidSum", "long")	// San	上一天非加液收入总和 4
-                        .add("previousLiquidSum", "long")	// Sbn  上一天非加液收入总和 5
-                        .add("outAmount", "long")			// Sxn  上一天固定收入的支出总和 6
-                        .add("previousRateSum", "long")		// Syn  上一天利率收入的支出总和 7
-                        .add("whichDay", "int")				// n 执行到第几天的标志 8
-                        .add("whichDate", "date")			// date  日期 9
+                        .add("totalAmount", "long")            // a 固定总金额 0
+                        .add("totalDays", "int")                // d 总天数 1
+                        .add("noLiquidRate", "double")        // k 非加液利率 2
+                        .add("liquidRate", "double")            // q 加液利率 3
+                        .add("previousNoLiquidSum", "long")    // San	上一天非加液收入总和 4
+                        .add("previousLiquidSum", "long")    // Sbn  上一天非加液收入总和 5
+                        .add("outAmount", "long")            // Sxn  上一天固定收入的支出总和 6
+                        .add("previousRateSum", "long")        // Syn  上一天利率收入的支出总和 7
+                        .add("whichDay", "int")                // n 执行到第几天的标志 8
+                        .add("whichDate", "date")            // date  日期 9
                         ;
+
                 return new StructType()
-                        .add("curr", aType)
-                        .add("next", aType);
+                        .add("tempArray", ArrayType.apply(
+                                new StructType()
+                                        .add("curr", aType)
+                                        .add("next", aType))
+                        );
             }
 
             @Override
@@ -268,83 +272,61 @@ public class My {
             @Override
             public void initialize(MutableAggregationBuffer buffer) {
 
-                Row row = RowFactory.create(0L, 0, 0.0, 0.0, 0L, 0L, 0L, 0L, 0, null);
-                buffer.update(0, row);
-                buffer.update(1, row);
+                List<Row> list = Lists.newArrayList();
+                buffer.update(0, list.toArray(new Row[0]));
             }
 
             @Override
             public void update(MutableAggregationBuffer buffer, Row input) {
 
-                buffer.update(0, buffer.get(1));
+                List<Row> tempList = Lists.newArrayList(buffer.getList(0));
 
                 Timestamp nextTimestamp = input.getAs(0);
                 Date date2 = new Date(nextTimestamp.getTime());
-
-                Row currType = buffer.getAs(0);
-                // 四个定值 总金额、总天数、加液利率、非加液利率
-                long sum = currType.getAs(IDX_TOTALAMOUNT);							// 固定总金额		a
-                int days = currType.getAs(IDX_TOTALDAYS);							// 总天数		d
-                double noLiquidRate = currType.getAs(IDX_NOLIQUIDRATE);				// 非加液利率		k
-                double liquidRate = currType.getAs(IDX_LIQUIDRATE);					// 加液利率		q
-                long preNoLiquidSum = currType.getAs(IDX_PREVIOUSNOLIQUIDSUM);		// 上一天非加液收入总和	San
-                long preLiquidSum = currType.getAs(IDX_PREVIOUSLIQUIDSUM);			// 上一天加液收入总和		Sbn
-                long outAmount = currType.getAs(IDX_OUTAMOUNT);						// 上一天固定收入的支出总和	Sxn
-                long preRateSum = currType.getAs(IDX_PREVIOUSRATESUM);				// 上一天固定收入的支出总和	Syn
-                int n = currType.getAs(IDX_WHICHDAY);								// n1
-                Date date1 = currType.getAs(IDX_WHICHDATE);							// date1
-
-                int n2 = 0;
-                if (!Objects.isNull(date1)) {
-                    // 这里不能用compareTo方法计算两行间隔的时间
-                    long differDays = date1.toLocalDate().until(date2.toLocalDate(), ChronoUnit.DAYS);
-                    n2 = n + (int) differDays;
-                    // n2 = Math.min(differN2, days);
-                }
-
-                long liquidIncome = 0L;
-                long noLiquidIncome = 0L;
-
                 String subject = input.getAs(2);
-                if (Strings.isNullOrEmpty(subject)) {
-                    logger.warn("科目为空!!!");
-                } else if (subject.equals("主营业务收入.收入.加液") || subject.equals("主营业务收入.退款.加液")) {
-                    liquidIncome = input.getAs(1);
-                } else {
-                    noLiquidIncome = input.getAs(1);
-                }
-
-                for (int i = n; i < n2; i++) {
-
-                    long res = Math.round(1.0 * sum / days * (i+1) - outAmount);			// Sxn`
-                    outAmount += res;
-
-                    long res2 = Math.round(preNoLiquidSum * noLiquidRate + preLiquidSum * liquidRate - preRateSum);
-                    preRateSum += res2;
-                }
-
-                preNoLiquidSum += noLiquidIncome;		// San`
-                preLiquidSum += liquidIncome;			// Sbn`
-
+                Long amount = input.getAs(1);
                 Long nextSum = input.getAs(3);
                 Double nextNoLiquidRate = input.getAs(4);
                 Double nextLiquidRate = input.getAs(5);
-                Integer nextDays = input.getAs(6);
+                Timestamp endDay = input.getAs(6);
+
+                int nextDays = 0;
+                if (!Objects.isNull(endDay)) {
+                    nextDays = (int) nextTimestamp.toLocalDateTime().toLocalDate().until(endDay.toLocalDateTime().toLocalDate(), ChronoUnit.DAYS);
+                }
+
+                // b1为false代表是有效策略行
                 boolean b1 = Objects.isNull(nextSum) && Objects.isNull(nextNoLiquidRate)
-                        && Objects.isNull(nextLiquidRate) && Objects.isNull(nextDays);
-                if(!b1){
+                        && Objects.isNull(nextLiquidRate) && Objects.isNull(endDay);
+
+                // 如果是结束日, 执行营收计算
+                if (!Objects.isNull(endDay) && nextTimestamp.getTime() == endDay.getTime()) {
+                    b1 = true;
+                }
+
+                if (!b1) {
                     nextSum = MoreObjects.firstNonNull(nextSum, 0L);
                     nextDays = MoreObjects.firstNonNull(nextDays, 0);
                     nextNoLiquidRate = MoreObjects.firstNonNull(nextNoLiquidRate, 0.0);
                     nextLiquidRate = MoreObjects.firstNonNull(nextLiquidRate, 0.0);
-                    Row nextRow2 = RowFactory.create(nextSum, nextDays, nextNoLiquidRate, nextLiquidRate, preNoLiquidSum,
-                            preLiquidSum, outAmount, preRateSum, n2, date2);
-                    buffer.update(1, nextRow2);
-                }else {
-                    Row nextRow = RowFactory.create(sum, days, noLiquidRate, liquidRate, preNoLiquidSum,
-                            preLiquidSum, outAmount, preRateSum, n2, date2);
-                    buffer.update(1, nextRow);
+
+                    Row row1 = RowFactory.create(0L, 0, 0.0, 0.0, 0L, 0L, 0L, 0L, 0, null);
+                    Row row2 = RowFactory.create(nextSum, nextDays, nextNoLiquidRate, nextLiquidRate, 0L, 0L, 0L, 0L, 0, date2);
+                    Row tempAddRow = RowFactory.create(row1, row2);
+                    tempList.add(tempAddRow);
+                } else {
+                    for (int i = 0; i < tempList.size(); i++) {
+                        Row currType = tempList.get(i).getAs(1);
+                        Row nextRow = updateNextRow(currType, nextTimestamp, subject, amount);
+                        if (Objects.isNull(nextRow)) {
+                            tempList.remove(i--);
+                        } else {
+                            Row evaRow = RowFactory.create(currType, nextRow);
+                            tempList.set(i, evaRow);
+                        }
+                    }
                 }
+                buffer.update(0, tempList);
             }
 
             @Override
@@ -355,48 +337,106 @@ public class My {
             @Override
             public Object evaluate(Row buffer) {
 
-                Row currType = buffer.getAs(0);
-                Row nextType = buffer.getAs(1);
+                List<Row> result = Lists.newArrayList();
+                List<Row> bufferList = buffer.getList(0);
+                
+                for (int k = 0; k < bufferList.size(); k++) {
+                    Row bufferListItem = bufferList.get(k);
+                    Row currType = bufferListItem.getAs(0);
+                    Row nextType = bufferListItem.getAs(1);
+
+                    // 四个定值 总金额、总天数、加液利率、非加液利率
+                    long sum = currType.getAs(IDX_TOTALAMOUNT);                            // 固定总金额		a
+                    int days = currType.getAs(IDX_TOTALDAYS);                            // 总天数		d
+                    double noLiquidRate = currType.getAs(IDX_NOLIQUIDRATE);                // 非加液利率		k
+                    double liquidRate = currType.getAs(IDX_LIQUIDRATE);                    // 加液利率		q
+                    long preNoLiquidSum = currType.getAs(IDX_PREVIOUSNOLIQUIDSUM);        // 上一天非加液收入总和	San
+                    long preLiquidSum = currType.getAs(IDX_PREVIOUSLIQUIDSUM);            // 上一天加液收入总和		Sbn
+                    long outAmount = currType.getAs(IDX_OUTAMOUNT);                        // 上一天固定收入的支出总和	Sxn
+                    long preRateSum = currType.getAs(IDX_PREVIOUSRATESUM);                // 上一天固定收入的支出总和	Syn
+                    int n = currType.getAs(IDX_WHICHDAY);                                // n1
+                    Date date1 = currType.getAs(IDX_WHICHDATE);                            // date1
+                    int n2 = nextType.getAs(IDX_WHICHDAY);                                // n2
+
+                    for (int i = n; i < n2; i++) {
+
+                        long res1 = Math.round(1.0 * sum / days * (i + 1) - outAmount);            // Sxn`
+                        long res2 = Math.round(preNoLiquidSum * noLiquidRate + preLiquidSum * liquidRate - preRateSum);
+
+                        if (preRateSum + res2 < sum) {
+                            result.add(RowFactory.create(Date.valueOf(date1.toLocalDate()), res1));
+                        } else if (preRateSum > sum) {
+                            result.add(RowFactory.create(Date.valueOf(date1.toLocalDate()), res2));
+                        } else {
+                            long res = preRateSum + res2 - outAmount;
+                            result.add(RowFactory.create(Date.valueOf(date1.toLocalDate()), res));
+                        }
+
+                        outAmount += res1;
+                        preRateSum += res2;
+                        date1 = Date.valueOf(date1.toLocalDate().plusDays(1));
+                    }
+                }
+
+                return JavaConverters.asScalaIteratorConverter(result.iterator()).asScala().toSeq();
+            }
+
+            private Row updateNextRow(Row currType, Timestamp ts, String subject, Long amount) {
+                Date date2 = new Date(ts.getTime());
 
                 // 四个定值 总金额、总天数、加液利率、非加液利率
-                long sum = currType.getAs(IDX_TOTALAMOUNT);							// 固定总金额		a
-                int days = currType.getAs(IDX_TOTALDAYS);							// 总天数		d
-                double noLiquidRate = currType.getAs(IDX_NOLIQUIDRATE);				// 非加液利率		k
-                double liquidRate = currType.getAs(IDX_LIQUIDRATE);					// 加液利率		q
-                long preNoLiquidSum = currType.getAs(IDX_PREVIOUSNOLIQUIDSUM);		// 上一天非加液收入总和	San
-                long preLiquidSum = currType.getAs(IDX_PREVIOUSLIQUIDSUM);			// 上一天加液收入总和		Sbn
-                long outAmount = currType.getAs(IDX_OUTAMOUNT);						// 上一天固定收入的支出总和	Sxn
-                long preRateSum = currType.getAs(IDX_PREVIOUSRATESUM);				// 上一天固定收入的支出总和	Syn
-                int n = currType.getAs(IDX_WHICHDAY);								// n1
-                Date date1 = currType.getAs(IDX_WHICHDATE);							// date1
-                int n2 = nextType.getAs(IDX_WHICHDAY);								// n2
+                long sum = currType.getAs(IDX_TOTALAMOUNT);                            // 固定总金额		a
+                int days = currType.getAs(IDX_TOTALDAYS);                            // 总天数		d
+                double noLiquidRate = currType.getAs(IDX_NOLIQUIDRATE);                // 非加液利率		k
+                double liquidRate = currType.getAs(IDX_LIQUIDRATE);                    // 加液利率		q
+                long preNoLiquidSum = currType.getAs(IDX_PREVIOUSNOLIQUIDSUM);        // 上一天非加液收入总和	San
+                long preLiquidSum = currType.getAs(IDX_PREVIOUSLIQUIDSUM);            // 上一天加液收入总和		Sbn
+                long outAmount = currType.getAs(IDX_OUTAMOUNT);                        // 上一天固定收入的支出总和	Sxn
+                long preRateSum = currType.getAs(IDX_PREVIOUSRATESUM);                // 上一天分成收入的支出总和	Syn
+                int n = currType.getAs(IDX_WHICHDAY);                                // n1
+                Date date1 = currType.getAs(IDX_WHICHDATE);                            // date1
 
-                List<Row> result = Lists.newArrayList();
+                if (n == days) {
+                    return null;
+                }
+
+                int n2 = 0;
+                if (!Objects.isNull(date1)) {
+                    long differDays = date1.toLocalDate().until(date2.toLocalDate(), ChronoUnit.DAYS);
+                    n2 = n + (int) differDays;
+                }
+
+                long liquidIncome = 0L;
+                long noLiquidIncome = 0L;
+
+                if (Strings.isNullOrEmpty(subject)) {
+                    logger.warn("科目为空!!!");
+                } else if (subject.equals("主营业务收入.收入.加液") || subject.equals("主营业务收入.退款.加液")) {
+                    liquidIncome = amount;
+                } else {
+                    noLiquidIncome = amount;
+                }
+
                 for (int i = n; i < n2; i++) {
 
-                    long res1 = Math.round(1.0 * sum / days * (i + 1) - outAmount);            // Sxn`
+                    long res = Math.round(1.0 * sum / days * (i + 1) - outAmount);            // Sxn`
+                    outAmount += res;
+
                     long res2 = Math.round(preNoLiquidSum * noLiquidRate + preLiquidSum * liquidRate - preRateSum);
-
-                    if (preRateSum + res2 < sum) {
-                        result.add(RowFactory.create(Date.valueOf(date1.toLocalDate()), res1));
-                    } else if (preRateSum > sum) {
-                        result.add(RowFactory.create(Date.valueOf(date1.toLocalDate()), res2));
-                    } else {
-                        long res = preRateSum + res2 - outAmount;
-                        result.add(RowFactory.create(Date.valueOf(date1.toLocalDate()), res));
-                    }
-
-                    outAmount += res1;
                     preRateSum += res2;
-                    date1 = Date.valueOf(date1.toLocalDate().plusDays(1));
                 }
-                return result.toArray(new Row[0]);
+
+                preNoLiquidSum += noLiquidIncome;        // San`
+                preLiquidSum += liquidIncome;            // Sbn`
+
+                Row nextRow = RowFactory.create(sum, days, noLiquidRate, liquidRate, preNoLiquidSum,
+                        preLiquidSum, outAmount, preRateSum, n2, date2);
+                return nextRow;
             }
+
         }.apply(expr(instantTime), expr(income), expr(subject), expr(fixedAmount),
                 expr(noLiquidRate), expr(liquidRate), expr(endDate));
     }
-
-
     
     public static Column 阶梯分成(String instantTime, String income, String subject,
                               String rate2, String liquidRate) {
--- src/test/java/FuncTest.java
+++ src/test/java/FuncTest.java
@@ -5,11 +5,10 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
 import com.google.common.collect.ImmutableList;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.catalyst.encoders.RowEncoder;
 import org.apache.spark.sql.expressions.Window;
+import org.apache.spark.sql.types.StructType;
 import org.junit.Assert;
 import org.junit.Test;
 import scala.Tuple2;
@@ -169,21 +168,41 @@ public class FuncTest {
 
     @Test
     public void fengtanErrorTest3() {
-        Dataset<Tuple3<String, String, Long>> ds = spark.createDataset(ImmutableList.of(
-                Tuple3.apply("2019-12-30 07:00", null, null),
-                Tuple3.apply("2019-12-30 08:00", "主营业务收入.收入.加液", 2L),
-                Tuple3.apply("2020-01-01 08:00", "主营业务收入.收入.加液", 2L),
-                Tuple3.apply("2020-04-09 07:00", null, null)
-        ), Encoders.tuple(Encoders.STRING(), Encoders.STRING(), Encoders.LONG()));
-        Dataset<Row> df = ds
-                .withColumn("ex", My.分摊分成("timestamp(_1)", "_3", "_2",
-                        "if(isnull(_2),4,null)",
-                        "if(isnull(_2),0.4,null)", "if(isnull(_2),0.8,null)",
-                        "if(isnull(_2),8,null)")
-                        .over(Window.orderBy("_1")))
+        Dataset<Row> src = spark.createDataset(ImmutableList.of(
+                RowFactory.create("2019-12-30 08:00", "主营业务收入.收入.加液", 2L, null, null, null, null),
+                RowFactory.create("2020-01-01 08:00", "主营业务收入.收入.加液", 2L, null, null, null, null)
+        ), RowEncoder.apply(new StructType()
+                .add("ts", "string")
+                .add("科目", "string")
+                .add("金额", "long")
+                .add("固定", "long")
+                .add("比例1", "double")
+                .add("比例2", "double")
+                .add("结束日", "string")));
+
+        Dataset<Row> df = src
+                .union(spark.sql("select '2019-12-30 07:00', null, null, 4, 0.4, 0.8, '2020-04-09 07:00'"))
+                .union(spark.sql("select '2020-04-09 07:00', null, null, null, null, null, '2020-04-09 07:00'"))
+                .withColumn("ex", My.分摊分成(
+                        "timestamp(ts)", "`金额`", "`科目`",
+                        "`固定`", "`比例1`", "`比例2`", "timestamp(`结束日`)")
+                        .over(Window.orderBy("ts")))
                 .selectExpr("explode(ex) ex")
                 .selectExpr("ex.*");
         assertEquals(101, df.count());
+
+
+        df = src
+                .union(spark.sql("select '2019-12-30 07:00', null, null, 4, 0.4, 0.8, '2020-04-09 07:00'"))
+                .union(spark.sql("select '2019-12-30 07:00', null, null, 4, 0.4, 0.8, '2020-04-09 07:00'"))
+                .union(spark.sql("select '2020-04-09 07:00', null, null, null, null, null, '2020-04-09 07:00'"))
+                .withColumn("ex", My.分摊分成(
+                        "timestamp(ts)", "`金额`", "`科目`",
+                        "`固定`", "`比例1`", "`比例2`", "timestamp(`结束日`)")
+                        .over(Window.orderBy("ts")))
+                .selectExpr("explode(ex) ex")
+                .selectExpr("ex.*");
+        assertEquals(202, df.count());
     }
 
 
@@ -218,20 +237,41 @@ public class FuncTest {
 
     @Test
     public void fengtanTest3() {
-        Dataset<Tuple3<String, String, Long>> ds = spark.createDataset(ImmutableList.of(
-                Tuple3.apply("2020-04-01 07:00", null, null),
-                Tuple3.apply("2020-04-01 08:00", "主营业务收入.收入.加液", 2L),
-                Tuple3.apply("2020-04-02 08:00", "主营业务收入.收入.加液", 2L),    // [4.1, 1]
-                Tuple3.apply("2020-04-03 08:00", "主营业务收入.收入.X", 4L),        // [4.2, 2]
-                Tuple3.apply("2020-04-05 16:00", "主营业务收入.收入.X", 4L),        // [4.3, 1],[4.4, 1]
-                Tuple3.apply("2020-04-06 08:00", "主营业务收入.收入.加液", 4L),    // [4.5, 1]
-                Tuple3.apply("2020-04-07 08:00", "主营业务收入.收入.加液", 2L),    // [4.6, 2]
-                Tuple3.apply("2020-04-09 07:00", null, null)                    // [4.7, 1],[4.8, 1]
-        ), Encoders.tuple(Encoders.STRING(), Encoders.STRING(), Encoders.LONG()));
-        Dataset<Row> df = ds
-                .withColumn("ex", My.分摊分成("timestamp(_1)", "_3", "_2",
-                        "if(isnull(_2),10,null)", "null", "null", "if(isnull(_2),8,null)")
-                        .over(Window.orderBy("_1")))
+        Dataset<Row> src = spark.createDataset(ImmutableList.of(
+                RowFactory.create("2020-04-01 08:00", "主营业务收入.收入.加液", 2L, null, null, null, null),
+                RowFactory.create("2020-04-02 08:00", "主营业务收入.收入.加液", 2L, null, null, null, null),
+                RowFactory.create("2020-04-03 08:00", "主营业务收入.收入.X", 4L, null, null, null, null),
+                RowFactory.create("2020-04-05 16:00", "主营业务收入.收入.X", 4L, null, null, null, null),
+                RowFactory.create("2020-04-06 08:00", "主营业务收入.收入.加液", 4L, null, null, null, null),
+                RowFactory.create("2020-04-07 08:00", "主营业务收入.收入.加液", 2L, null, null, null, null)
+        ), RowEncoder.apply(new StructType()
+                .add("ts", "string")
+                .add("科目", "string")
+                .add("金额", "long")
+                .add("固定", "long")
+                .add("比例1", "double")
+                .add("比例2", "double")
+                .add("结束日", "string")));
+
+        Dataset<Row> df = src
+                .union(spark.sql("select '2020-04-01 07:00', null, null, 10L, null, null, '2020-04-09 07:00'"))
+                .union(spark.sql("select '2020-04-01 07:00', null, null, null, 0.4, 0.8, '2020-04-09 07:00'"))
+                .union(spark.sql("select '2020-04-09 07:00', null, null, null, null, null, '2020-04-09 07:00'"))
+                .withColumn("ex", My.分摊分成(
+                        "timestamp(ts)", "`金额`", "`科目`",
+                        "`固定`", "`比例1`", "`比例2`", "timestamp(`结束日`)")
+                        .over(Window.orderBy("ts")))
+                .selectExpr("explode(ex) ex")
+                .selectExpr("ex.*");
+        assertEquals(16, df.count());
+
+        df = src
+                .union(spark.sql("select '2020-04-01 07:00', null, null, 10L, null, null, '2020-04-09 07:00'"))
+                .union(spark.sql("select '2020-04-09 07:00', null, null, null, null, null, '2020-04-09 07:00'"))
+                .withColumn("ex", My.分摊分成(
+                        "timestamp(ts)", "`金额`", "`科目`",
+                        "`固定`", "`比例1`", "`比例2`", "timestamp(`结束日`)")
+                        .over(Window.orderBy("ts")))
                 .selectExpr("explode(ex) ex")
                 .selectExpr("ex.*");
         assertEquals(8, df.count());
@@ -239,12 +279,13 @@ public class FuncTest {
         assertEquals(2L, df.where("date='2020-04-02'").first().getLong(1));
         assertEquals(2L, df.where("date='2020-04-06'").first().getLong(1));
 
-        df = ds
-                .withColumn("ex", My.分摊分成("timestamp(_1)", "_3", "_2",
-                        "null",
-                        "if(isnull(_2),0.4,null)", "if(isnull(_2),0.8,null)",
-                        "if(isnull(_2),8,null)")
-                        .over(Window.orderBy("_1")))
+        df = src
+                .union(spark.sql("select '2020-04-01 07:00', null, null, null, 0.4, 0.8, '2020-04-09 07:00'"))
+                .union(spark.sql("select '2020-04-09 07:00', null, null, null, null, null, '2020-04-09 07:00'"))
+                .withColumn("ex", My.分摊分成(
+                        "timestamp(ts)", "`金额`", "`科目`",
+                        "`固定`", "`比例1`", "`比例2`", "timestamp(`结束日`)")
+                        .over(Window.orderBy("ts")))
                 .selectExpr("explode(ex) ex")
                 .selectExpr("ex.*");
         assertEquals(8, df.count());
@@ -255,12 +296,13 @@ public class FuncTest {
         assertEquals(4L, df.where("date='2020-04-06'").first().getLong(1));
         assertEquals(1L, df.where("date='2020-04-07'").first().getLong(1));
 
-        df = ds
-                .withColumn("ex", My.分摊分成("timestamp(_1)", "_3", "_2",
-                        "if(isnull(_2),4,null)",
-                        "if(isnull(_2),0.4,null)", "if(isnull(_2),0.8,null)",
-                        "if(isnull(_2),8,null)")
-                        .over(Window.orderBy("_1")))
+        df = src
+                .union(spark.sql("select '2020-04-01 07:00', null, null, 4, 0.4, 0.8, '2020-04-09 07:00'"))
+                .union(spark.sql("select '2020-04-09 07:00', null, null, null, null, null, '2020-04-09 07:00'"))
+                .withColumn("ex", My.分摊分成(
+                        "timestamp(ts)", "`金额`", "`科目`",
+                        "`固定`", "`比例1`", "`比例2`", "timestamp(`结束日`)")
+                        .over(Window.orderBy("ts")))
                 .selectExpr("explode(ex) ex")
                 .selectExpr("ex.*");
         assertEquals(8, df.count());
