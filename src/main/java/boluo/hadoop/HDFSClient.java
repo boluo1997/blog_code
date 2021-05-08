e377365ba58dd35894ddfe457f4a8e3f712c4f54
boluo1997 <1109928059@qq.com>
2021-05-08 21:52:35 +0800

    hadoop

--- src/main/java/boluo/hadoop/HDFSClient.java
+++ src/main/java/boluo/hadoop/HDFSClient.java
@@ -3,13 +3,11 @@ package boluo.hadoop;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.junit.Test;
 
 import java.net.URI;
 
 public class HDFSClient {
 
-    @Test
     public void put() throws Exception {
         FileSystem fileSystem = FileSystem.get(URI.create("hdfs://hadoop102:9000"), new Configuration(), "boluo");
 
