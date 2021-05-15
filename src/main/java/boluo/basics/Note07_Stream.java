e0167e02b979a749efd90dadd71f771b8754bd39
boluo1997 <1109928059@qq.com>
2021-05-15 15:28:37 +0800

    generate方式创建Stream

--- src/main/java/boluo/basics/Note07_Stream.java
+++ src/main/java/boluo/basics/Note07_Stream.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.IntSupplier;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -77,6 +78,9 @@ public class Note07_Stream {
             }
         };
         IntStream.generate(fib).limit(10).forEach(System.out::println);
+
+        String str = Stream.generate(() -> "?").limit(5).collect(Collectors.joining(","));
+        System.out.println(str);
     }
 
 }
