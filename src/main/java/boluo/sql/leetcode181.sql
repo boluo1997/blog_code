c7a4bf55bebf10d420c1fc12b8141cc8860b60b8
boluo1997 <1109928059@qq.com>
2021-05-15 22:33:46 +0800

    leetcode181: 超过经理收入的员工

---
+++ src/main/java/boluo/sql/leetcode181.sql
@@ -0,0 +1,25 @@
+-- employee 表包含所有员工，他们的经理也属于员工。每个员工都有一个 Id，此外还有一列对应员工的经理的 Id。
++----+-------+--------+-----------+
+| Id | name  | Salary | ManagerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | NULL      |
+| 4  | Max   | 90000  | NULL      |
++----+-------+--------+-----------+
+-- 给定 employee 表，编写一个 SQL 查询，该查询可以获取收入超过他们经理的员工的姓名。
+-- 在上面的表格中，Joe 是唯一一个收入超过他的经理的员工。
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+
+select e1.name as Employee
+from employee e1
+where e1.salary > (
+    select salary
+    from employee e2
+    where e1.managerId = e2.Id
+);
+
