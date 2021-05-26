72a7c9219cbb9192d8013248041bddbd07f8137c
dingchao <1109928059@qq.com>
2021-05-26 18:32:06 +0800

    HttpURLConnection

--- src/main/java/tuling/net/MallTest.java
+++ src/main/java/tuling/net/MallTest.java
@@ -5,6 +5,15 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+
+/**
+ * 首先 URL restURL = new URL(url);这其中的url就是需要调的目标接口地址,URL类是java.net.*下的类，这个不陌生。
+ * setRequestMethod("POST");请求方式是有两个值进行选择，一个是GET,一个是 POST，选择对应的请求方式
+ * setDoOutput(true);setDoInput(true);
+ * setDoInput(): 设置是否向httpUrlConnection输出，因为这个是 post请求，参数要放在http正文内，因此需要设为true, 默认是false;
+ * setDoOutput(): 设置是否从httpUrlConnection读入，默认情况下是 true;
+ * setAllowUserInteraction(); allowUserInteraction 如果为 true，则在允许用户交互（例如弹出一个验证对话框）的上下文中对此 URL 进行检查。
+ */
 public class MallTest {
 
 	public String load(String url) throws Exception {
