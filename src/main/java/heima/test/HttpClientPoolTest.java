d86e0b2126cd8212bdaa94c38983a18c138cf7db
boluo1997 <1109928059@qq.com>
2021-06-05 21:44:22 +0800

    HttpClient连接池

---
+++ src/main/java/heima/test/HttpClientPoolTest.java
@@ -0,0 +1,56 @@
+package heima.test;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpClientPoolTest {
+    public static void main(String[] args) {
+
+        // 创建连接池管理器
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+
+        // 设置最大连接数
+        cm.setMaxTotal(100);
+
+        // 设置每个主机的最大连接数
+        cm.setDefaultMaxPerRoute(10);
+
+        doGet(cm);
+        doGet(cm);
+    }
+
+    private static void doGet(PoolingHttpClientConnectionManager cm) {
+        // 不是每次创建新的HttpClient对象, 而是从连接池中获取HttpClient对象
+        CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(cm).build();
+        HttpGet httpGet = new HttpGet("http://www.itcast.cn");
+
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(httpGet);
+            if (response.getStatusLine().getStatusCode() == 200) {
+                String content = EntityUtils.toString(response.getEntity(), "utf-8");
+                System.out.println(content.length());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // 不能关闭HttpClient, 由连接池管理
+            // httpClient.close();
+        }
+    }
+
+
+}
+
