7b5884ba4799d73109f565212f19ad261cc4c322
boluo1997 <1109928059@qq.com>
2021-06-06 15:59:11 +0800

    处理document对象

--- src/test/java/jsoup/JsoupFirstTest.java
+++ src/test/java/jsoup/JsoupFirstTest.java
@@ -3,6 +3,7 @@ package jsoup;
 import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.Test;
 
 import java.io.File;
@@ -44,6 +45,34 @@ public class JsoupFirstTest {
 
         String title = doc.getElementsByTag("title").first().text();
         System.out.println(title);
+
+    }
+
+    @Test
+    public void testDom() throws Exception {
+
+        // 解析文件, 获取document对象
+        Document doc = Jsoup.parse(new File("D:\\IDEA_projects\\blog_code\\doc\\test\\jsoup-test.html"), "utf-8");
+
+        // 获取元素
+
+        // 1. 根据id来获取元素
+        Element element1 = doc.getElementById("people");
+        System.out.println("获取到的元素内容是: " + element1.text());
+
+        // 2.根据标签来获取元素
+        Element element2 = doc.getElementsByTag("span").first();
+        System.out.println("获取到的元素内容是: " + element2.text());
+
+        // 3.根据class来获取元素
+        Element element3 = doc.getElementsByClass("all a_js on").first();
+        System.out.println("获取到的元素内容是: " + element3.text());
+
+        // 4.根据属性来获取元素
+        Element element4 = doc.getElementsByAttribute("abc").first();
+        Element element5 = doc.getElementsByAttributeValue("abc", "456").first();
+        System.out.println("获取到的元素内容是: " + element4.text());
+        System.out.println("获取到的元素内容是: " + element5.text());
 
     }
 
