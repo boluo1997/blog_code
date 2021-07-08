508819f87da61805cbcf0769b7338057e8d627f4
dingchao <1109928059@qq.com>
2021-07-08 19:04:48 +0800

    遗传算法

--- src/main/java/boluo/algorithm/Chromosome.java
+++ src/main/java/boluo/algorithm/Chromosome.java
@@ -2,25 +2,19 @@ package boluo.algorithm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @Description: 基因遗传染色体
  */
 public class Chromosome {
 
-	private boolean[] gene;//基因序列
-	private double score;//对应的函数得分
-
-	public double getScore() {
-		return score;
-	}
-
-	public void setScore(double score) {
-		this.score = score;
-	}
+	private boolean[] gene;    // 基因序列
+	private double score;    // 对应的函数得分
 
 	/**
-	 * @param size 随机生成基因序列
+	 * @param size
+	 * @Description 随机生成基因序列 基因的每一个位置是0还是1, 这里采用完全随机的方式实现
 	 */
 	public Chromosome(int size) {
 		if (size <= 0) {
@@ -32,101 +26,16 @@ public class Chromosome {
 		}
 	}
 
-	/**
-	 * 生成一个新基因
-	 */
-	public Chromosome() {
-
-	}
-
-	/**
-	 * @param c
-	 * @return
-	 * @Author:lulei
-	 * @Description: 克隆基因
-	 */
-	public static Chromosome clone(final Chromosome c) {
-		if (c == null || c.gene == null) {
-			return null;
-		}
-		Chromosome copy = new Chromosome();
-		copy.initGeneSize(c.gene.length);
-		for (int i = 0; i < c.gene.length; i++) {
-			copy.gene[i] = c.gene[i];
-		}
-		return copy;
-	}
-
-	/**
-	 * @param size
-	 * @Author:lulei
-	 * @Description: 初始化基因长度
-	 */
-	private void initGeneSize(int size) {
+	public void initGeneSize(int size) {
 		if (size <= 0) {
 			return;
 		}
 		gene = new boolean[size];
 	}
 
-
-	/**
-	 * @param p1
-	 * @param p2
-	 * @Author:lulei
-	 * @Description: 遗传产生下一代
-	 */
-	public static List<Chromosome> genetic(Chromosome p1, Chromosome p2) {
-		if (p1 == null || p2 == null) { //染色体有一个为空，不产生下一代
-			return null;
-		}
-		if (p1.gene == null || p2.gene == null) { //染色体有一个没有基因序列，不产生下一代
-			return null;
-		}
-		if (p1.gene.length != p2.gene.length) { //染色体基因序列长度不同，不产生下一代
-			return null;
-		}
-		Chromosome c1 = clone(p1);
-		Chromosome c2 = clone(p2);
-		//随机产生交叉互换位置
-		int size = c1.gene.length;
-		int a = ((int) (Math.random() * size)) % size;
-		int b = ((int) (Math.random() * size)) % size;
-		int min = a > b ? b : a;
-		int max = a > b ? a : b;
-		//对位置上的基因进行交叉互换
-		for (int i = min; i <= max; i++) {
-			boolean t = c1.gene[i];
-			c1.gene[i] = c2.gene[i];
-			c2.gene[i] = t;
-		}
-		List<Chromosome> list = new ArrayList<Chromosome>();
-		list.add(c1);
-		list.add(c2);
-		return list;
-	}
-
-	/**
-	 * @param num
-	 * @Author:lulei
-	 * @Description: 基因num个位置发生变异
-	 */
-	public void mutation(int num) {
-		//允许变异
-		int size = gene.length;
-		for (int i = 0; i < num; i++) {
-			//寻找变异位置
-			int at = ((int) (Math.random() * size)) % size;
-			//变异后的值
-			boolean bool = !gene[at];
-			gene[at] = bool;
-		}
-	}
-
 	/**
 	 * @return
-	 * @Author:lulei
-	 * @Description: 将基因转化为对应的数字
+	 * @Description 将基因整体转换成对应的数字, 比如 101 -> 5
 	 */
 	public int getNum() {
 		if (gene == null) {
@@ -141,4 +50,22 @@ public class Chromosome {
 		}
 		return num;
 	}
+
+	/**
+	 * @param num
+	 * @Description 基因 num个位置发生变异 对于变异的位置, 这里采用完全随机的方式实现, 变异原则 0->1 1->0
+	 */
+	public void mutation(int num) {
+		// 允许变异
+		int size = gene.length;
+		for (int i = 0; i < num; i++) {
+			// 寻找变异位置
+			int at = ((int) (Math.random() * size)) % size;
+
+			// TODO 变异后的值
+		}
+	}
+
 }
+
+
