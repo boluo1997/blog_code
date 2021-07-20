7ba069a273db853f1f80e4d86de46700dff58fbe
dingchao <1109928059@qq.com>
2021-07-20 18:43:13 +0800

    按照偏移量切割字符串

--- src/main/java/boluo/basics/Note16_String.java
+++ src/main/java/boluo/basics/Note16_String.java
@@ -1,8 +1,10 @@
 package boluo.basics;
 
+import com.clearspring.analytics.util.Lists;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -64,9 +66,22 @@ public class Note16_String {
 
 	@Test
 	public void func2() {
-		String str1 = "1,2,3,4";            // 切割成 1,    2,     3,     4
-		String str2 = "1,2,3,";            // 切割成 1,    2,     3,     ""
-		String str3 = "1,2,,3";            // 切割成 1,    2,     ,      3
+		String str = "在每年8.31日前结清下一期管理费，甲方提供正规增值税专用发票，后续新增设备管理费不满一个月按一个月计算，超过一个月不满一年按实际月份计算。（台数按实际投放台数登记）\n" +
+				"浙大新宇5校区大合同，总履约保证金10000元。保洁、水电费规定查看服务协议。\n" +
+				"2020.2.4留学生门店拆出。\n" +
+				"2021年3月调增洗烘设备3+2=5台，管理费按1400元/台/年核算，调增流程：XMTZ202101001。\n" +
+				"2021年6月调增洗衣机1台，管理费按1400元/台/年核算，调增流程：XMTZ20210600001。\n" +
+				"截止2020-08-31，每年费用50400.00元。（洗烘设备数量24台+12台=36台）\n" +
+				"截止2021-02-28，每年费用54600.00元。（洗烘设备数量26台+13台=39台）\n" +
+				"截止2021-05-31，每年费用61600.00元。（洗烘设备数量29台+15台=44台）";
 
+		List<String> result = Lists.newArrayList();
+		Pattern pattern = Pattern.compile("截止\\d{4}-\\d{2}-\\d{2}，每年费用([\\d\\.]+)元.*");
+
+		// 切割成   ***截止**, 截止**, 截止**, "" 四段
+		// TODO
+
+		System.out.println(result);
 	}
+
 }
