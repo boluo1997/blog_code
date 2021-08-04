3096d2de728833710a3e0e3269a27de3263a009e
dingchao <1109928059@qq.com>
2021-08-04 18:39:35 +0800

    guava cache

---
+++ src/main/java/boluo/model/User.java
@@ -0,0 +1,54 @@
+package boluo.model;
+
+public class User {
+
+	long userId;
+
+	String name;
+
+	int age;
+
+	String gender;
+
+	public User() {
+	}
+
+	public User(long userId, String name, int age, String gender) {
+		this.userId = userId;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+}
