2fa9074212a05039b0d0f692cdae36345aca235e
dingchao <1109928059@qq.com>
2021-08-14 17:10:55 +0800

    Data Sources

--- src/main/java/boluo/spark/Doc02_Data_Sources.java
+++ src/main/java/boluo/spark/Doc02_Data_Sources.java
@@ -1,20 +1,22 @@
 package boluo.spark;
 
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SaveMode;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.*;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Doc02_Data_Sources {
 
-	public static void main(String[] args) {
+	static SparkSession spark = SparkSession
+			.builder()
+			.appName("Java Spark SQL basic example")
+			.master("local[*]")
+			.config("spark.some.config.option", "some-value")
+			.getOrCreate();
 
-		SparkSession spark = SparkSession
-				.builder()
-				.appName("Java Spark SQL basic example")
-				.master("local[*]")
-				.config("spark.some.config.option", "some-value")
-				.getOrCreate();
+	public static void main(String[] args) {
 
 //		Dataset<Row> userDs = spark.read().load("examples/src/main/resources/users.parquet");
 //		userDs.select("name", "favorite_color").write().save("nameAndFavColors.parquet");
@@ -64,5 +66,45 @@ public class Doc02_Data_Sources {
 				.saveAsTable("users_partitioned_bucketed");
 	}
 
+	@Test
+	public void func1() {
+
+		// load parquet
+		Dataset<Row> peopleDs = SparkSession.active().read().parquet("examples/src/main/resources/people.parquet");
+		peopleDs.registerTempTable("parquetFile");
+		Dataset<Row> namesDf = SparkSession.active().sql("select name from parquetFile where age between 13 and 19");
+		Dataset<String> namesDs = namesDf.map(
+				(MapFunction<Row, String>) row -> "Name: " + row.getString(0),
+				Encoders.STRING()
+		);
+		namesDs.show();
+	}
+
+	@Test
+	public void func2() {
+
+		List<String> jsonData = Arrays.asList(
+				"{\"name\":\"Yin\",\"address\":{\"city\":\"Columbus\",\"state\":\"Ohio\"}}");
+		Dataset<String> anotherPeopleDataset = spark.createDataset(jsonData, Encoders.STRING());
+		anotherPeopleDataset.show(false);
+	}
+
+	@Test
+	public void func3() {
+
+		// loading JDBC source
+		Dataset<Row> jdbcDs = SparkSession.active().read()
+				.format("jdbc")
+				.option("url", "127.0.0.1:3306")
+				.option("dbtable", "japan_video")
+				.option("user", "root")
+				.option("password", "root")
+				.load();
+		jdbcDs.show(false);
+	}
 
 }
+
+
+
+
