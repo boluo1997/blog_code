9605097475d4114f1d1991556b5317a5be677afc
dingchao <1109928059@qq.com>
2021-08-16 18:58:43 +0800

    update table data

--- src/main/java/boluo/delta/Doc01_Quickstart.java
+++ src/main/java/boluo/delta/Doc01_Quickstart.java
@@ -1,10 +1,13 @@
 package boluo.delta;
 
 import io.delta.tables.DeltaTable;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 import static org.apache.spark.sql.functions.*;
 
@@ -37,8 +40,34 @@ public class Doc01_Quickstart {
 		// Delta Lake provides programmatic APIs to conditional update, delete and merge(upsert) data into tables
 		DeltaTable deltaTable = DeltaTable.forPath("./examples/delta-table");
 
+		// Update every even value by adding 100 to it
+		deltaTable.update(
+				expr("id % 2 == 0"),
+				new HashMap<String, Column>() {{
+					put("id", expr("id + 100"));
+				}}
+		);
 
+		// Delete every even value
+		deltaTable.delete(expr("id % 2 == 0"));
+
+		// Upsert (merge) new data
+		Dataset<Row> newData = spark.range(0, 20).toDF();
+		deltaTable.as("oldData")
+				.merge(
+						newData.as("newData"),
+						"oldData.id = newData.id"
+				)
+				.whenMatched()
+				.update(
+						new HashMap<String, Column>(){{
+							put("id", expr("newData.id"));
+						}})
+				.whenNotMatched()
+				.insertAll()
+				.execute();
 	}
+
 
 }
 
